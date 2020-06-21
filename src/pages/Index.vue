231216b42482ab822183b1bb8df3d9f3b1c0046b
JosephPire <he201388@students.ephec.be>
2020-06-21 03:49:29 +0200

    feat(Layout): rework index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,95 +1,309 @@
 <template>
-  <Layout :disableScroll="true">
-<!--    <div class="container sm:px-0 mx-auto overflow-x-hidden">-->
-    <div class="container sm:px-0 mx-auto overflow-x-hidden">
+  <Layout :disableScroll="false">
 
-      <div class=" pt-4 sm:pt-16 pb-4 sm:pb-16 mx-4 sm:-mx-4">
+     <!-- Waves -->
+     <div id="waves-container">
+         <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+             <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                 <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                     <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
+                     <path d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z" opacity="0.100000001"></path>
+                     <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" id="Path-4" opacity="0.200000003"></path>
+                 </g>
+                 <g id="last-wave-light" transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                     <path d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"></path>
+                 </g>
+                 <g id="last-wave-dark" transform="translate(-4.000000, 76.000000)" fill="beige" fill-rule="nonzero">
+                     <path d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"></path>
+                 </g>
+             </g>
+         </svg>
+     </div>
 
-<!--        <div class="sm:mx-16 sm:mb-8">-->
-          <h3 class="flex justify-center md:mt-12">
-            <span>I like to play with :</span>
-          </h3>
-<!--        </div>-->
-      </div>
+     <!--About me -->
+     <section id="about-me" class="border-b py-8 mx-auto sm:px-0">
+         <div class="container max-w-5xl mx-auto m-8">
+             <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center">About me</h1>
+             <div class="w-full mb-4">
+                 <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+             </div>
 
-        <div class="mb-10">
-          <VueSlickCarousel v-bind="techCarouselSettings">
-            <div  v-for="(tech, index) in $page.allTech.edges" :key="tech.node.id" :index="index">
-              <figure class="flex justify-center mb-10 align">
-                <g-image :src="tech.node.logo" :alt="tech.node.title" :title="tech.node.title" immediate="true"/>
-              </figure>
-            </div>
-          </VueSlickCarousel>
-        </div>
+             <div class="flex flex-wrap">
+                 <div class="w-5/6 sm:w-1/2 p-6">
+                     <h3 class="text-3xl font-bold leading-none mb-3 ">Personality</h3>
+                     <p class="text-gray-600 mb-8">I am a quite guy, who like to think and learn; curious by nature, I love to understand how thing works in order to acquire new skills and push myself to be better. If i can work by myself, I always like to meet new people and collaborators, or working within a team.<p>
+                     <p class="text-red-500 underline">Improve myself</p>
 
-        <div class="hidden sm:block sm:mx-16 sm:mt-16 sm:pb-4 sm:mb-8">
-          <h6 class="text-center">
-            Feel free to take a look at my humble work or to leave me a message :)
-          </h6>
-        </div>
+                 </div>
+                 <div class="w-full sm:w-1/2 p-6">
+                     <figure class="w-full md:w-4/5 z-50" >
+                         <PersonalityLogo class="about-logo" />
+                     </figure>
+                 </div>
+             </div>
 
-      </div>
+
+             <div class="flex flex-wrap flex-col-reverse sm:flex-row">
+                 <div class="w-full sm:w-1/2 p-6 mt-6">
+                     <figure class="w-full md:w-4/5 z-50" >
+                         <MotivationLogo class="about-logo" />
+                     </figure>
+                 </div>
+                 <div class="w-full sm:w-1/2 p-6 mt-6">
+                     <div class="align-middle">
+                         <h3 class="text-3xl font-bold leading-none mb-3 ">Motivations</h3>
+                         <p class="text-gray-600 mb-8">Iam looking for new challenges; I really hope i can find a job where i could be useful, applying what i learned to help others, build bridge across peoples, and share with my siblings.<p>
+                         <p class="text-red-500 underline">Live in harmony</p>
+                     </div>
+                 </div>
+
+             </div>
+         </div>
+     </section>
+
+     <!-- Main Skills -->
+     <section id="main-stack" class="border-b py-8 mx-auto sm:px-0">
+
+         <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+
+             <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center">Main Skills</h1>
+             <div class="w-full mb-4">
+                 <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+             </div>
+
+             <div  v-for="skill in $page.allSkill.edges" :key="skill.node.id" class=" w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                 <g-link :to="skill.node.path">
+                     <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+                         <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+                             <div class="w-full font-bold text-xl text-gray-800 px-6 text-center h-16 my-4">{{skill.node.title}}</div>
+                             <div class="text-gray-800 text-sm px-6 mb-5 h-20">
+                                 {{skill.node.shortDescription}}
+                             </div>
+                         </a>
+                     </div>
+                     <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+                         <div class="flex items-center justify-center">
+                             <div class="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-0 py-4 px-8 shadow-lg">More info</div>
+                         </div>
+                     </div>
+                 </g-link>
+             </div>
+
+         </div>
+
+     </section>
+
+     <!-- Featured techs -->
+     <section class="bg-gray-200 py-8">
+
+         <div class="container mx-auto px-2 pt-4 pb-12 text-gray-800">
+             <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Featured Techs</h1>
+             <div class="w-full mb-4">
+                 <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+             </div>
+
+             <div class="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4">
+
+                 <div v-for="(skill, index) in $page.allSkill.edges" :key="skill.node.id"
+                      v-bind:class="{
+                  'lg:w-1/4 rounded-none lg:rounded-l-lg' : index !== 1,
+                  'lg:w-1/3  sm:-mt-6 rounded-lg sm:-mt-6 shadow-lg z-10' : index === 1
+              }" class="flex flex-col w-5/6 mx-auto lg:mx-0 bg-white mt-4 b-white rounded">
+
+                     <div v-bind:class="{'text-gray-600' : index !== 1}" class="flex-1  overflow-hidden shadow">
+                         <g-link :to="skill.node.path">
+                             <div v-bind:class="{
+                          'border-b-4' : index !== 1,
+                          'w-full' :  index === 1,
+                      }" class="p-8 text-3xl font-bold text-center">{{skill.node.shortName}}</div>
+                         </g-link>
+                         <div v-if="index === 1" class="h-1 w-full gradient my-0 py-0 "></div>
+                         <ul  v-bind:class="{
+                          'text-sm' : index !== 1,
+                          'text-base font-bold' : index === 1
+                      }" class="w-full text-center">
+                             <g-link v-for="(tech, index) in skill.node.techs" :key="tech.id" :to="tech.path">
+                                 <li v-bind:class="{'border-b': index !== skill.node.techs.length-1 }" class="py-4">{{tech.title}}</li>
+                             </g-link>
+                         </ul>
+                     </div>
+                     <div class="flex-none mt-auto shadow p-6">
+                         <div v-bind:class="{
+                          'text-3xl text-gray-600 ' : index !== 1,
+                          'text-4xl' : index === 1
+                      }" class="w-full pt-6 font-bold text-center">{{skill.node.relatedProjects.totalCount}} <span class="text-base">{{getPluralizedLabel('project', skill.node.relatedProjects.totalCount)}} related</span></div>
+                         <div v-bind:class="{
+                          'text-3xl text-gray-600 ' : index !== 1,
+                          'text-4xl' : index === 1
+                      }" class="w-full pt-6 font-bold text-center">{{skill.node.relatedJobs.totalCount}} <span class="text-base">{{getPluralizedLabel('job', skill.node.relatedJobs.totalCount)}} related</span></div>
+                         <g-link :to="skill.node.path">
+                             <div class="flex items-center justify-center">
+                                 <div class="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">More info</div>
+                             </div>
+                         </g-link>
+                     </div>
+                 </div>
+
+             </div>
+
+         </div>
+
+     </section>
+
+     <!-- Call to Action -->
+     <div id="call-to-action-container">
+         <!-- Change the colour #f8fafc to match the previous section colour -->
+         <svg class="wave-top" viewBox="0 0 1439 147" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+             <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                 <g transform="translate(-1.000000, -14.000000)" fill-rule="nonzero">
+                     <g class="wave" fill="rgb(237, 242, 247)">
+                         <path d="M1440,84 C1383.555,64.3 1342.555,51.3 1317,45 C1259.5,30.824 1206.707,25.526 1169,22 C1129.711,18.326 1044.426,18.475 980,22 C954.25,23.409 922.25,26.742 884,32 C845.122,37.787 818.455,42.121 804,45 C776.833,50.41 728.136,61.77 713,65 C660.023,76.309 621.544,87.729 584,94 C517.525,105.104 484.525,106.438 429,108 C379.49,106.484 342.823,104.484 319,102 C278.571,97.783 231.737,88.736 205,84 C154.629,75.076 86.296,57.743 0,32 L0,0 L1440,0 L1440,84 Z"></path>
+                     </g>
+                     <g transform="translate(1.000000, 15.000000)" fill="#FFFFFF">
+                         <g transform="translate(719.500000, 68.500000) rotate(-180.000000) translate(-719.500000, -68.500000) ">
+                             <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
+                             <path d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z" opacity="0.100000001"></path>
+                             <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" opacity="0.200000003"></path>
+                         </g>
+                     </g>
+                 </g>
+             </g>
+         </svg>
+
+         <section class="container mx-auto text-center pt-6 pb-24">
+
+             <h1 class="w-full my-2 text-5xl text-white font-bold leading-tight text-center">Lets get in touch</h1>
+             <div class="w-full mb-4">
+                 <div class="h-1 mx-auto bg-gray-300 w-1/6 my-0 py-0 rounded-t"></div>
+             </div>
+
+             <h4 class="my-4 text-3xl text-white leading-tight">I might be able to help you</h4>
+
+             <g-link to="/contact">
+                 <div class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg inline-block">Send a message!</div>
+             </g-link>
+
+         </section>
+
+     </div>
+
   </Layout>
 </template>
 
 <page-query>
-  query {
-    allTech {
-      totalCount
+query {
+    allTech(filter: {title: {in: ["API Platform","Vuejs","tailwindcss"]}}) {
       edges {
         node {
           id
           title
-          path
+          shortDescription
           logo(width:64, height:64, fit:contain, background:"rgba(0,0,0,0)", quality:10)
+          path
         }
       }
+    },
+      allSkill(
+      filter: {id: {in: ["skill7","skill12", "skill13"]}},
+      sortBy: "id"
+    )
+    {
+     edges {
+        node {
+          id
+          title
+          path
+          shortName
+          shortDescription
+          relatedProjects: belongsTo(filter:{ typeName: {eq: Project}}){
+            totalCount
+      	  },
+          relatedJobs: belongsTo(filter:{ typeName: {eq: Job}}){
+            totalCount
+          },
+          techs {
+            title
+            path
+          }
+      	}
+      }
     }
-
   }
 </page-query>
 
 <script>
 
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+import PersonalityLogo from '~/assets/svg/undraw_thought_process_67my.svg?inline'
+import MotivationLogo from '~/assets/svg/undraw_team_spirit_hrr4.svg?inline'
+
+import pluralize from "pluralize";
+
 
 export default {
   metaInfo: {
     title: "Home"
   },
-  data() {
-    return {
-      techCarouselSettings: {
-        "arrows": false,
-        "dots": true,
-        "infinite": true,
-        "slidesToShow": 5,
-        "autoplay": true,
-        "speed": 2000,
-        "autoplaySpeed": 1000,
-        "swipeToSlide": true,
-        "responsive": [
-          {
-            "breakpoint": 768,
-            "settings": {
-              "slidesToShow": 5,
-            }
-          },
-          {
-            "breakpoint": 640,
-            "settings": {
-              "slidesToShow": 3,
-              "dots": false,
-            }
-          }
-        ]
-      },
-    }
+  methods: {
+    getPluralizedLabel(label, count) {
+        return pluralize(label, count);
+    },
   },
-  components: {
-    VueSlickCarousel
-  }
+  components : {
+      PersonalityLogo,
+      MotivationLogo
+  },
 };
 </script>
+
+<style scoped>
+
+    ul {
+        list-style: none;
+        margin: 0;
+    }
+
+    #waves-container {
+        background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
+    }
+    body[data-theme="dark"] #last-wave-light, body[data-theme="light"] #last-wave-dark  {
+        display: none;
+    }
+
+    body[data-theme="light"] #last-wave-light, body[data-theme="dark"] #last-wave-dark {
+        display: block;
+    }
+
+    body[data-theme="dark"] #waves-transition {
+        background-color: #191b1f;
+    }
+
+    body[data-theme="light"] #waves-transition {
+        background-color: white;
+    }
+
+    body[data-theme="dark"] #call-to-action-container, body[data-theme="dark"] #waves-container  {
+        background: linear-gradient(90deg,black 60%, #742a2a 250%);
+    }
+
+    body[data-theme="light"] #call-to-action-container, body[data-theme="light"] #waves-container  {
+        background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
+    }
+
+
+    body[data-theme="dark"] #about-me {
+        background-color: beige;
+    }
+    body[data-theme="dark"] #about-me h1, body[data-theme="dark"] #about-me h3 {
+        color: black;
+    }
+
+    body[data-theme="dark"] #main-stack .stack-card {
+        background-color: beige;
+    }
+
+    .about-logo {
+        width: 100%;
+        height: auto;
+    }
+
+</style>
