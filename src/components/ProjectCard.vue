070c214cc39e6f94dc1642563c0e8ff92387168c
JosephPire <he201388@students.ephec.be>
2020-06-30 19:29:13 +0200

    refactor(component): change aspect of the project card component

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -6,7 +6,7 @@
             <h2 class="text-center md:mt-8">
                 {{ project.title }}
             </h2>
-            <p class="text-center px-4">{{ project.shortDescription }}</p>
+            <p class="card-description text-center px-4">{{ project.shortDescription }}</p>
 
             <span class="info-button hover:underline text-white font-bold rounded-full my-8 py-4 px-8 shadow-lg text-center self-center">
                 More info
@@ -37,13 +37,21 @@
 
     body[data-theme="light"] .project-card{
         background-color: #e6e6e6;
+        .card-description{
+            color: #2f2e41;
+        }
     }
     body[data-theme="dark"] .project-card{
         background-color: #2f2e41;
+        .card-description{
+            color: #7e7d99;
+        }
     }
 
     .info-button {
         background-color: #f50057;
     }
+
+
 
 </style>
