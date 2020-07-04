c0d3debdc097e2a69968b425b38d1dd138f6e8d1
JosephPire <he201388@students.ephec.be>
2020-07-04 22:53:19 +0200

    feat(Component): Create btn component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,27 +1,42 @@
 <template>
-    
+    <button class="font-bold rounded-full py-4 px-8 shadow-lg hover:underline focus:outline-none" :class="classes">
+        {{label}}
+    </button>
 </template>
 
 <script>
     export default {
         name: "Button",
-        properties: {
+        props: {
             label: {
                 type: String,
                 required: true
             },
             classes: {
-                type: String,
                 required: false
-            },
-            link: {
-                type: String,
-                required: false,
-            },
+            }
         }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+    .bg-gradient, .bg-rose {
+        color: white;
+    }
+    .bg-rose {
+        background-color: #f50057;
+    }
+    body[data-theme="dark"] {
+        .bg-gradient {
+            background: linear-gradient(90deg,#1a202c 60%, #c53030 250%);
+        }
+    }
+
+    body[data-theme="light"] {
+         .bg-gradient {
+            background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
+        }
+    }
 
 </style>
