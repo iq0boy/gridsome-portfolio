02d29efc0aa40358c24ebec33ee83ddb6af515b6
JosephPire <he201388@students.ephec.be>
2020-07-04 17:33:34 +0200

    refactor(Tailwind): Cleanup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,19 +25,15 @@ module.exports = {
       auto: '1 1 auto',
       initial: '0 1 auto',
       none: 'none',
-      'post': '1 1 300px',
-      '100': '1 1 100%',
-      'post-large-content': '0 1 361px',
     },
     zIndex: {
       '-10': '-10',
       '0': 0,
       '10': 10,
       '20': 20,
+      '25': 25,
       '30': 30,
       '40': 40,
-      '50': 50,
-      '25': 25,
       '50': 50,
       '75': 75,
       '100': 100,
@@ -48,23 +44,10 @@ module.exports = {
       container: false
     },
     extend: {},
-    radialGradients: {
-      shapes: { // defaults to this value
-        'default': 'ellipse',
-      },
-      sizes: { // defaults to this value
-        'default': '',
-      },
-      positions: { // defaults to these values
-        'default': 'center',
-        't': 'top'
-      },
-      colors: { // defaults to {}
-        'gray-to-black': ['rgba(25, 25, 25, 1)', 'rgba(8, 8, 8, 1)', 'rgba(0, 0, 0, 1)']
-      },
-    },
   },
-  variants: {},
+  variants: {
+    zIndex: ['responsive', 'hover'],
+  },
   plugins: [
     function ({
       addComponents
